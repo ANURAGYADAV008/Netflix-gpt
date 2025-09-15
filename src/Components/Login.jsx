7f2fd4bbe7186b7026b4a9c40ae7a01b08129eba
Anurag Yadav <jaysinghanurag321@gmail.com>
2025-09-15 17:25:08 +0530

    useAuth Hook

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,176 +1,91 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import { useRef, useState } from "react";
-import ValidationData from "../Utils/validation";
-import { auth } from "../Utils/Firebase";
-import {
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  updateProfile,
-} from "firebase/auth";
-import { useNavigate } from "react-router-dom";
-import { useDispatch } from "react-redux";
-import { addUser } from "../Utils/userSlice";
+import useHandleAuth from "../Utils/useHandleAuth";
 
 const Login = () => {
-  const navigate=useNavigate()
-  const dispatch=useDispatch();
   const [isSignInForm, setIsSignInForm] = useState(true);
-  const [errormessage, setErrormessage] = useState(null);
-  
 
   const email = useRef(null);
   const password = useRef(null);
   const name = useRef(null);
 
-  const handleButtonClick = () => {
-    const message = ValidationData(
-      email.current?.value,
-      password.current?.value,
-      name.current?.value
-    );
-    setErrormessage(message);
-
-    if (message) return; // stop if validation failed
-
-    if (!isSignInForm) {
-      // ✅ Signup logic
-      createUserWithEmailAndPassword(
-        auth,
-        email.current?.value,
-        password.current?.value
-      )
-        .then((userCredential) => {
-          const user = userCredential.user;
-          console.log("Signed up user ->", user);
-          updateProfile(user,{
-            displayName:name.current?.value,
-            photoURL:"https://avatars.githubusercontent.com/u/150878739?v=4"
-          })
-          .then(()=>{
-          const { uid, email, displayname } = auth.currentUser;
-                          dispatch(addUser({
-                              uid: user.uid,
-                              email: user.email,
-                              displayName: user.displayName
-                          }));
-          navigate("/browse")
-          })
-          .catch((error)=>{
-            setErrormessage(error)
-          })
-        })
-        .catch((error) => {
-          const errCode = error.code;
-          const errorMessage = error.message;
-          if(errorMessage!==""){
-            setErrormessage("Invalid Credientials");
-          }
-          
-        });
-    } else {
-      // ✅ Sign In logic
-      signInWithEmailAndPassword(
-        auth,
-        email.current?.value,
-        password.current?.value
-      )
-        .then((userCredential) => {
-          const user = userCredential.user;
-          console.log("Signed in user ->", user);
-          navigate("/browse");
-        })
-        .catch((error) => {
-          const errorCode = error.code;
-          const errorMessage = error.message;
-          if(errorMessage!==""){
-          setErrormessage("Invalid Credientials");
-          }
-        });
-    }
-  };
-
-  const toggleSignInForm = () => {
-    setIsSignInForm(!isSignInForm);
-  };
+  const { errormessage, handleAuth } = useHandleAuth(email, password, name);
 
   return (
-    <div>
-      <div className="relative h-screen w-screen bg-black text-white">
-        {/* Header */}
-        <Header />
+    <div className="relative h-screen w-screen bg-black text-white">
+      <Header />
 
-        {/* Background Image */}
-        <div className="absolute inset-0">
-          <img
-            className="w-full h-full object-cover"
-            src="https://assets.nflxext.com/ffe/siteui/vlv3/0b0dad79-ad4d-42b7-b779-8518da389976/web/IN-en-20250908-TRIFECTA-perspective_0647b106-80e1-4d25-9649-63099752b49a_large.jpg"
-            alt="Background"
-          />
-          <div className="absolute inset-0 bg-black opacity-50"></div>
-        </div>
+      {/* Background */}
+      <div className="absolute inset-0">
+        <img
+          className="w-full h-full object-cover"
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/0b0dad79-ad4d-42b7-b779-8518da389976/web/IN-en-20250908-TRIFECTA-perspective_0647b106-80e1-4d25-9649-63099752b49a_large.jpg"
+          alt="Background"
+        />
+        <div className="absolute inset-0 bg-black opacity-50"></div>
+      </div>
 
-        {/* Login Form */}
-        <div className="absolute inset-0 flex items-center justify-center">
-          <form
-            onSubmit={(e) => e.preventDefault()}
-            className="backdrop bg-black/50 p-12 rounded-lg shadow-lg w-96 flex flex-col space-y-4"
-          >
-            <h1 className="text-3xl font-bold mb-6">
-              {isSignInForm ? "Sign In" : "Sign Up"}
-            </h1>
+      {/* Form */}
+      <div className="absolute inset-0 flex items-center justify-center">
+        <form
+          onSubmit={(e) => e.preventDefault()}
+          className="bg-black/50 p-12 rounded-lg shadow-lg w-96 flex flex-col space-y-4"
+        >
+          <h1 className="text-3xl font-bold mb-6">
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </h1>
 
-            {!isSignInForm && (
-              <input
-                type="text"
-                ref={name}
-                placeholder="User Name"
-                className="p-3 rounded-md bg-gray-800/70 text-white focus:outline-none"
-              />
-            )}
-
+          {!isSignInForm && (
             <input
               type="text"
-              placeholder="Email"
-              ref={email}
+              ref={name}
+              placeholder="User Name"
               className="p-3 rounded-md bg-gray-800/70 text-white focus:outline-none"
             />
+          )}
 
-            <input
-              type="password"
-              placeholder="Password"
-              ref={password}
-              className="p-3 rounded-md bg-gray-800/70 text-white focus:outline-none"
-            />
+          <input
+            type="text"
+            ref={email}
+            placeholder="Email"
+            className="p-3 rounded-md bg-gray-800/70 text-white focus:outline-none"
+          />
 
-            <button
-              type="submit"
-              onClick={handleButtonClick}
-              className="bg-red-600 hover:bg-red-700 transition-colors duration-300 p-3 rounded-md font-semibold"
+          <input
+            type="password"
+            ref={password}
+            placeholder="Password"
+            className="p-3 rounded-md bg-gray-800/70 text-white focus:outline-none"
+          />
+
+          <button
+            type="submit"
+            onClick={() => handleAuth(isSignInForm)}
+            className="bg-red-600 hover:bg-red-700 transition-colors duration-300 p-3 rounded-md font-semibold"
+          >
+            {isSignInForm ? "Sign In" : "Sign Up"}
+          </button>
+
+          <p className="text-sm text-red-700 font-bold">{errormessage}</p>
+
+          <p className="text-sm text-gray-300">
+            {isSignInForm ? "New to MovieFlix ?" : "Already a user ? "}
+            <span
+              className="text-white cursor-pointer hover:underline"
+              onClick={() => setIsSignInForm(!isSignInForm)}
             >
-              {isSignInForm ? "Sign In" : "Sign Up"}
-            </button>
-            <p className="text-sm text-red-700 font-bold">{errormessage}</p>
-
-            <p className="text-sm text-gray-300">
-              {isSignInForm ? "New to MovieFlix ?" : "Already a user ? "}
-              <span
-                className="text-white cursor-pointer hover:underline"
-                onClick={toggleSignInForm}
-              >
-                {isSignInForm ? "Sign Up" : "Sign In"}
-              </span>
-              .
-            </p>
-          </form>
-        </div>
+              {isSignInForm ? "Sign Up" : "Sign In"}
+            </span>
+            .
+          </p>
+        </form>
       </div>
 
-      <div>
-        <Footer />
-      </div>
+      <Footer />
     </div>
   );
 };
 
 export default Login;
+
